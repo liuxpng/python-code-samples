b54f67891ca193df6504d9f073a1d5bd77ff00b6
liuxpng <liuxpng@foxmail.com>
2024-07-02 23:46:28 +0800

    feat: python-cookbook chapter 13(11)

---
+++ python-cookbook/chapter_13/11_logging.py
@@ -0,0 +1,8 @@
+import logging
+import logging.config
+
+logging.basicConfig(level=logging.INFO)
+
+logging.info('this is a info message')
+logging.warning('this is a warning message')
+logging.error('this is a error message')
